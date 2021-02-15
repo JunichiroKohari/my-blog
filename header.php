7c370ad406b11a0e90c765b9f8824f8dac3b44c1
JunichiroKohari <junichiro@kohari.jp>
2021-02-15 19:33:09 +0900

    ユーザ名（表示名）を動的に出力

--- header.php
+++ header.php
@@ -34,12 +34,16 @@
 		</nav>
 		<div class="top-img-div">
 			<div class="profile-div">
+				<?php
+					global $current_user;
+					get_currentuserinfo();
+				?>
 				<div class="profile-icon-div">
 					<img src="<?php bloginfo('template_directory'); ?>/img/profile-icon.png" class="profile-icon-img" alt="プロフィールアイコン">
-					<span class="my-name">Junichiro Kohari</span>
+					<span class="my-name"><?php echo $current_user->display_name; ?></span>
 				</div>
 				<div class="profile-txt-div">
-					<p>ご訪問ありがとうございます！Junichiro Kohariと申します。<br>大阪府内でSEとして働くかたわら、Web制作のお仕事も行っております。<br>このブログではIT業界で働く僕が、ITの技術や日々の業務で学んだこと、プライベートでやっている趣味に関することを発信していきます。</p>
+					<p>ご訪問ありがとうございます！<?php echo $current_user->display_name; ?>と申します。<br>大阪府内でSEとして働くかたわら、Web制作のお仕事も行っております。<br>このブログではIT業界で働く僕が、ITの技術や日々の業務で学んだこと、プライベートでやっている趣味に関することを発信していきます。</p>
 					<p class="last-txt">詳しいプロフィールは<a href="#" class="link-to-profile">こちら</a></p>
 				</div>
 			</div>
