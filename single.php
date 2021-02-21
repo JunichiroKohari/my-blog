17c8f18389eb3602bf988b90ef477f826317578a
JunichiroKohari <junichiro@kohari.jp>
2021-02-21 22:16:06 +0900

    記事詳細ページ　サイドバー定位置に配置

--- single.php
+++ single.php
@@ -7,31 +7,33 @@
 			<?php the_post_thumbnail( 'easiestwp-hero' ); ?>
 		</div>
 	<?php endif; ?>
-	<div class="main-and-aside">
-		<main class="main"></main>
-			<div class="box-generic">
-				<h1 class="box-heading box-heading-article"><?php the_title(); ?></h1>
-				<div class="box-content">
-					<article class="entry">
-						<?php the_content(); ?>
-					</article>
-					<div class="meta-data">
-						<time class="meta meta-entry-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
-						<p class="meta meta-author"><?php the_author_posts_link(); ?></p>
-						<p class="meta meta-cat"><?php the_category( ', ' ); ?></p>
-						<p class="meta meta-tag"><?php the_tags(); ?></p>
+	<div class="contents">
+		<div class="main-and-aside">
+			<main class="main">
+				<div class="box-generic">
+					<h1 class="box-heading box-heading-article"><?php the_title(); ?></h1>
+					<div class="box-content">
+						<article class="entry">
+							<?php the_content(); ?>
+						</article>
+						<div class="meta-data">
+							<time class="meta meta-entry-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
+							<p class="meta meta-author"><?php the_author_posts_link(); ?></p>
+							<p class="meta meta-cat"><?php the_category( ', ' ); ?></p>
+							<p class="meta meta-tag"><?php the_tags(); ?></p>
+						</div>
+						<?php the_post_navigation( array(
+							'prev_text' => '前の記事：%title',
+							'next_text' => '次の記事：%title',
+						) ); ?>
 					</div>
-					<?php the_post_navigation( array(
-						'prev_text' => '前の記事：%title',
-						'next_text' => '次の記事：%title',
-					) ); ?>
 				</div>
-			</div>
+			</main>
 			<?php if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif; ?>
+			<?php get_sidebar(); ?>
 		</div>
-		<?php get_sidebar(); ?>
 	</div>
 <?php endwhile; ?>
 
