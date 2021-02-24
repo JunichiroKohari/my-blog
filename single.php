c6eceabe261f679a313bd9dabb76e84b66c60796
JunichiroKohari <junichiro@kohari.jp>
2021-02-24 21:14:33 +0900

    記事詳細ページ　タイトル、メタ情報、本文　スタイル調整

--- single.php
+++ single.php
@@ -1,32 +1,36 @@
 <?php get_header(); ?>
 
 <div class="contents">
-	<div class="main-and-aside">
+	<div class="main-and-aside single">
 		<main class="main">
 		<?php while ( have_posts() ) : ?>
 			<?php the_post(); ?>
 			<?php if ( has_post_thumbnail() ) : ?>
-				<div class="eyecatch">
+				<div class="eyecatch-div">
 					<?php the_post_thumbnail( 'large' ); ?>
 				</div>
 			<?php endif; ?>
-			<div class="">
-				<h1 class=""><?php the_title(); ?></h1>
-				<div class="">
-					<article class="">
-						<?php the_content(); ?>
-					</article>
-					<div class="meta-data">
-						<time class="" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日'); ?></time>
-						<p class=""><?php the_category( ' ' ); ?></p>
-						<p class=""><?php the_tags( ' ' ); ?></p>
-					</div>
-					<?php the_post_navigation( array(
-						'prev_text' => '前の記事：%title',
-						'next_text' => '次の記事：%title',
-					) ); ?>
+			<h2 class="article-title"><?php the_title(); ?></h1>
+			<div class="article-metainfo">
+				<div class="time-div">
+					<i class="fas fa-calendar-alt"></i>
+					<time class="postedtime" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日'); ?></time>
+				</div>
+				<div class="cat-and-tag-div">
+					<i class="fas fa-folder-open"></i>
+					<?php the_category( ' ' ); ?>
+					<i class="fas fa-tag"></i>
+					<?php the_tags( ' ' ); ?>
 				</div>
 			</div>
+			<article class="article">
+				<?php the_content(); ?>
+			</article>
+			<?php the_post_navigation( array(
+				'screen_reader_text'=>' ',
+				'prev_text' => '前の記事：%title',
+				'next_text' => '次の記事：%title',
+			) ); ?>
 			<?php if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif; ?>
