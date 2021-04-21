8af531e48e909202a27b0d2ba36135b7cc97c430
JunichiroKohari <junichiro@kohari.jp>
2021-04-21 22:15:17 +0900

    各class付与

--- page-profile.php
+++ page-profile.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="margin-from-header">
+<main class="margin-from-header">
     <h2 class="profile-title">プロフィール</h2>
     <figure class="profile-icon-div">
         <img src="<?php bloginfo('template_directory'); ?>/img/profile-icon.png" class="profile-icon-img" alt="プロフィールアイコン">
@@ -10,13 +10,13 @@
     <p>大阪府内でSEとして働くかたわら、Web制作のお仕事も行っております。</p>
     <p class="profile-item__text-syumi">趣味は、外国人の友達とチャットをすることです。ポーランド人やウルグアイ人など、ネットで出会った友達と週末によくチャットをしています。<br>普段当たり前のようにネットでコミュニケーションを取っていますが、はるか遠く離れた所にいる人と瞬時にやりとりができるってすごいことですよね。<br>私は、ネットによって人と人とが繋がるという体験を仕事でも実現させていきたいなと思っています。</p>
     <p class="profile-item__text-contactme">ちょっとした相談やお悩み事の相談でも構いません。<br>ご気軽にご連絡ください。</p>
-    <h3>開発技術</h3>
-    <div>
+    <h3 class="technologies-title">開発技術</h3>
+    <div class="technology-div">
         <dt>OS</dt>
         <dd>windows10</dd>
         <dd>mac os Catalina</dd>
     </div>
-    <h3>概要</h3>
+    <h3 class="about-title">概要</h3>
     <div>
         <dt>屋号</dt>
         <dd>xxxxx</dd>
@@ -30,7 +30,7 @@
         <dd>Wordpress構築/改修</dd>
         <dd>Webデザイン/ワイヤーフレーム制作</dd>
     </div>
-    <button>お問合せはこちら</button>
-</div>
+    <button class="contact-btn">お問合せはこちら</button>
+</main>
 
 <?php get_footer();
