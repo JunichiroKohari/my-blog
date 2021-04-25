6058172c218af6063d86c4907475dff56a9785db
JunichiroKohari <junichiro@kohari.jp>
2021-04-25 20:43:35 +0900

    プライバシーポリシー作成完

---
+++ page-privacypolicy.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<main class="main-no-aside">
+    <h2 class="page-title">プライバシーポリシー</h2>
+    <section class="privacypolicy-section">
+        <?php
+            $group = SCF::get('privacypolicy');
+            foreach ($group as $fields ) {
+        ?>
+            <h3 class="text_title"><?php echo $fields['text_title']; ?></h3>
+            <p class="text_content"><?php echo $fields['text_content']; ?></p>
+        <?php } ?>
+    </section>
+    <div class="btn-div">
+        <button class="btn"><i class="far fa-envelope"></i>　お問合せはこちら</button>
+    </div>
+</main>
+
+<?php get_footer();
