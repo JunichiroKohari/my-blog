df810f0bcb21a5565f9579c1d5e0d6763099e0db
JunichiroKohari <junichiro@kohari.jp>
2021-05-01 22:24:51 +0900

    お問い合わせ画面修正中

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 /* ヘッダー */
 // ハンバーガーメニュー
-$('.hamb-button').on('click', function() {
-    $('.hamb-button').toggleClass('close-button');
+$('#hamb-button').on('click', function() {
+    $('#hamb-button').toggleClass('close-button');
     $('.nav').toggleClass('hamb-nav');
 });
 $('.nav-link').on('click', function() {
-    $('.hamb-button').toggleClass('close-button');
+    $('#hamb-button').toggleClass('close-button');
     $('.nav').toggleClass('hamb-nav');
 });
 
 /* よくある質問ページ */
 // 質問カテゴリのテキストが空の場合、要素非表示
-var quetion_category = $('.faq-section').children('.question-category');
+var quetion_category = $('#faq-section').children('#question-category');
 $.each (quetion_category, function (i, val) {
     if (val !== null &&　$(val).text() == '') {
         $(val).css('display', 'none');
@@ -22,17 +22,41 @@ $.each (quetion_category, function (i, val) {
 
 /* お問い合わせページ */
 // 送信ボタン押下時、メッセージ表示
-$('.submit').modaal({
+var modaalObj = {
     type: 'confirm',
     confirm_button_text: 'OK',
     confirm_cancel_button_text: '',
     confirm_title: 'お問い合わせいただきありがとうございます！',
     confirm_content: '<p>3営業日以内に回答いたします。</p>',
-});
+    // before_open: errorCheck,
+    should_open: true,
+};
+$('#submit').on('click', function () {
+    // errorCheck();
+    // if ($('#name').val() !== '' && $('#email').val() !== '' && $('#message').val() !== '') {
+    //     if (!$('#check').prop('checked')) {
+    //         alert('プライバシーポリシーをご確認いただき、チェックボックスにチェックを入れて下さい。');
+    //     } else {
+    //         $('#submit').modaal(modaalObj);
+    //     }
+    // }
+})
+// 必須項目入力チェック
+// function errorCheck() {
+//     if ($('#name').val() == '' || $('#email').val() == '' || $('#message').val() == '') {
+//         alert('お名前、メールアドレス、メッセージは入力必須項目です。');
+//         modaalObj.should_open = false;
+//     } else if (!$('#check').prop('checked')) {
+//         alert('プライバシーポリシーをご確認いただき、チェックボックスにチェックを入れて下さい。');
+//         modaalObj.should_open = false;
+//     } else {
+//         modaalObj.should_open = true;
+//     }
+// }
 
 /* コメント欄 */
 // コメント欄名前必須チェック
-$('#submit').attr('required', true);
+// $('#submit').attr('required', true);
 
 /* スライダー */
 // $('.slider').slick({
--- page-contact.php
+++ page-contact.php
@@ -9,14 +9,14 @@
         <p class="contact-info">よくある質問は<a href="<?php echo esc_url(get_page_link( 103 )) ?>" class="link-to-profile">こちら</a></p>
         <form action="#" class="contact-form">
             <div class="input-item">
-                <span class="required">必須</span>
-                <label class="contact-label">お名前</label>
-                <input type="text" class="contact-input" placeholder="田中　太郎" required>
+                <label class="contact-label"><span class="required">必須</span><span class="contact-label-text">お名前</span></label>
+                <input id="name" type="text" class="contact-input" placeholder="田中　太郎" required>
+                <i class="fas fa-check-circle"></i>
             </div>
             <div class="input-item">
-                <span class="required">必須</span>
-                <label class="contact-label">メールアドレス</label>
-                <input type="email" class="contact-input" placeholder="hoge@huga.com" required>
+                <label class="contact-label"><span class="required">必須</span><span class="contact-label-text">メールアドレス</span></label>
+                <input id="email" type="email" class="contact-input" placeholder="hoge@huga.com" required>
+                <i class="fas fa-check-circle"></i>
             </div>
             <div class="input-item dropdown-div">
                 <span class="not-required">任意</span>
@@ -30,17 +30,16 @@
                 </select>
             </div>
             <div class="input-item">
-                <span class="required">必須</span>
-                <label class="contact-label">メッセージ</label>
-                <textarea name="contact-txt" class="contact-input contact-txt" placeholder="ご気軽にご入力ください。" required></textarea>
+                <label class="contact-label"><span class="required">必須</span><span class="contact-label-text">メッセージ</span><i class="fas fa-check-circle"></i></label>
+                <textarea id="message" name="contact-txt" class="contact-input contact-txt" placeholder="ご気軽にご入力ください。" required></textarea>
             </div>
             <div class="input-item checkbox-div">
-                <input id="check" name="check" type="checkbox" class="checkbox" />
+                <input id="check" name="check" type="checkbox" class="checkbox" required/>
                 <label for="check" class="checkbox-label"></label>
                 <a href="<?php echo esc_url(get_page_link( 75 )) ?>" class="link-to-profile">プライバシーポリシー</a>に同意する
             </div>
             <div class="input-item btn-div">
-                <button class="submit" onclick="void(0);">送信</button>
+                <button id="submit" class="submit" onclick="void(0);">送信</button>
             </div>
         </form>
     </section>
