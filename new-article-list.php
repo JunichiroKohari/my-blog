fee8cbab8bc18c07a9b6001874898fe2e27d2660
JunichiroKohari <junichiro@kohari.jp>
2021-05-20 10:53:12 +0900

    最新記事一覧修正

--- new-article-list.php
+++ new-article-list.php
@@ -1,28 +1,27 @@
 <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
     <?php $classes = array('new-articles', 'sp-version-article', 'news'); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class($classes); ?>>
-        <a href="<?php the_permalink(); ?>" class="thumbnail-link article-left-div">
+        <a href="<?php the_permalink(); ?>" class="thumbnail-link">
             <?php if (has_post_thumbnail()) : the_post_thumbnail( 'medium' ) ?>
             <?php else: ?>
             <img src="<?php echo esc_url( get_template_directory_uri() . '/img/no-image.png' ); ?>" alt="サムネイル画像なし" class="wp-post-image">
             <?php endif; ?>
         </a>
-        <div class="article-right-div">
+        <div class="article-info-div">
             <h2 class="article-title"><a href="<?php the_permalink(); ?>" class="article-title-link"><?php the_title(); ?></a></h2>
             <time class="postedtime" datetime="<?php the_time('Y-m-d'); ?>"><i class="fas fa-calendar-alt"></i><?php the_time('Y年m月d日'); ?></time>
             <?php if (has_category()) : ?>
-                <span>
-                    <i class="fas fa-folder-open"></i>
-                    <?php the_category( ' ' ); ?>
-                </span>
+            <span>
+                <i class="fas fa-folder-open"></i>
+                <?php the_category( ' ' ); ?>
+            </span>
             <?php endif; ?>
             <?php if (has_tag()) : ?>
-                <span>
-                    <i class="fas fa-tag"></i>
-                    <?php the_tags( ' ' ); ?>
-                </span>
+            <span>
+                <i class="fas fa-tag"></i>
+                <?php the_tags( ' ' ); ?>
+            </span>
             <?php endif; ?>
-            <?php echo get_the_excerpt(); ?>
         </div>
     </article>
 <?php endwhile; ?>
